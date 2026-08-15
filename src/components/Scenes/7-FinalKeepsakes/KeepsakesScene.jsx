31610d809c3afd02d5eddde2c0df32a2401b3dd3
unknown <gomathimusini@gmail.com>
2026-08-15 23:46:18 +0530

    force update keepsakes css import

--- src/components/Scenes/7-FinalKeepsakes/KeepsakesScene.jsx
+++ src/components/Scenes/7-FinalKeepsakes/KeepsakesScene.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import LetterModal from './LetterModal';
-import './keepsakes.css';
 
 export default function KeepsakesScene({ polaroidImg, activeKeepsake, onClose }) {
   return (
